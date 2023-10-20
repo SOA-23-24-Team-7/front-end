099e89fcd1da4c0e2f7163fcffe23337dbb950a1
Marko Cekerinac <marko.cekerinac@gmail.com>
2023-10-20 15:20:52 +0200

    feat: Map add marker

--- Explorer/src/app/shared/map/map.component.ts
+++ Explorer/src/app/shared/map/map.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
+import { MapService } from './map.service';
 
 @Component({
   selector: 'xp-map',
@@ -9,14 +10,14 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: any;
 
-  constructor() {}
+  constructor(private mapService:MapService) {}
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [45.2396, 19.8227],
       zoom: 13,
-    });
-
+    }
+    );
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
@@ -27,9 +28,58 @@ export class MapComponent implements AfterViewInit {
       }
     );
     tiles.addTo(this.map);
+    this.registerOnClick();
+    this.setRoute();
+  }
+
+  search(): void {
+    this.mapService.search('Strazilovska 19, Novi Sad').subscribe({
+      next: (result) => {
+        console.log(result);
+        L.marker([result[0].lat, result[0].lon])
+          .addTo(this.map)
+          .bindPopup('Pozdrav iz Strazilovske 19.')
+          .openPopup();
+      },
+      error: () => {},
+    });
+  }
+
+  setRoute(): void {
+    const routeControl = L.Routing.control({
+      waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+      router: L.routing.mapbox('TVOJ_MAPBOX_API_KEY', {profile: 'mapbox/walking'})
+    }).addTo(this.map);
+
+    routeControl.on('routesfound', function(e) {
+      var routes = e.routes;
+      var summary = routes[0].summary;
+      alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
+    });
+  }
+
+  registerOnClick(): void {
+    this.map.on('click', (e: any) => {
+      const coord = e.latlng;
+      const lat = coord.lat;
+      const lng = coord.lng;
+      this.mapService.reverseSearch(lat, lng).subscribe((res) => {
+        console.log(res.display_name);
+      });
+      console.log(
+        'You clicked the map at latitude: ' + lat + ' and longitude: ' + lng
+      );
+      const mp = new L.Marker([lat, lng]).addTo(this.map);
+      alert(mp.getLatLng());
+    });
   }
 
   ngAfterViewInit(): void {
+    let DefaultIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
     this.initMap();
   }
 }
---
+++ Explorer/src/app/shared/map/map.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+  constructor(private http: HttpClient) {}
+
+  search(street: string): Observable<any> {
+    return this.http.get(
+      'https://nominatim.openstreetmap.org/search?format=json&q=' + street
+    );
+  }
+
+  reverseSearch(lat: number, lon: number): Observable<any> {
+    return this.http.get(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&<params>`
+    );
+  }
+
+  //primer rada funkcije za nadmorsku visinu, treba izmeniti tako da prihvata nase paramtere za lat i long, izmenio sam samo ove lat i lon, a ovo 10,10 i 20,20 nisam ulazio sta je, vredi pogledati
+  //return this.http.get('https://api.open-elevation.com/api/v1/lookup?locations=10,10|20,20|41.161758,-8.583933') ovako je izgledala originalna lajna
+  getElevation(lat: number, lon: number): Observable<any> {
+    return this.http.get(`https://api.open-elevation.com/api/v1/lookup?locations=10,10|20,20|${lat},${lon}`)
+}
+}
