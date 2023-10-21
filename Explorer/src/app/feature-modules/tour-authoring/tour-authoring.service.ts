3425adff1c5b5c79cc53b725fc56356c0bcd3669
Jelena Kovač <jelena.kovac765@gmail.com>
2023-10-21 15:02:55 +0200

    feat: tourEquipment- addiing, removing and view of equipment on tour

--- Explorer/src/app/feature-modules/tour-authoring/tour-authoring.service.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-authoring.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
 import {Tour} from './model/tour.model';
+import { Equipment } from './model/equipment.model';
 import { Observable } from 'rxjs';
 import { environment } from 'src/env/environment';
 
@@ -26,4 +27,19 @@ export class TourAuthoringService {
     return this.http.put<Tour>(environment.apiHost + 'tour/' + tour.id, tour);
   }
 
+  getEquipment(): Observable<PagedResults<Equipment>>{
+    return this.http.get<PagedResults<Equipment>>(environment.apiHost + 'author/equipment');
+  }
+
+  getTourEquipment(id: number): Observable<PagedResults<Equipment>> {
+    return this.http.get<PagedResults<Equipment>>(environment.apiHost + 'tour/tourEquipment/' + id);
+  }
+
+  addTourEquipment(tourId:number, eqId:number): Observable<Tour> { 
+    return this.http.post<Tour>(environment.apiHost + 'tour/tourEquipment/' + tourId + '/' + eqId,{});
+  }
+
+  deleteTourEquipment(tourId:number, eqId:number):Observable<Tour> {
+    return this.http.delete<Tour>(environment.apiHost + 'tour/tourEquipment/' + tourId + '/' + eqId);
+  }
 }
