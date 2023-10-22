cdad33c94afc147a015181d5e3fa77e2a879dcdd
ukinolo <ykithethird@gmail.com>
2023-10-22 16:59:05 +0200

    feat: added tourist equipment selection view

--- Explorer/src/app/feature-modules/layout/layout.module.ts
+++ Explorer/src/app/feature-modules/layout/layout.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { RouterModule } from '@angular/router';
+import { TouristEquipmentSelectionComponent } from './tourist-equipment-selection/tourist-equipment-selection.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TouristEquipmentSelectionComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    TouristEquipmentSelectionComponent
   ]
 })
 export class LayoutModule { }
