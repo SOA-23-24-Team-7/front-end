28a3de372fb4f7ae7f713f4f0b90da3a4c446a65
Marko Cekerinac <marko.cekerinac@gmail.com>
2023-11-03 16:23:13 +0100

    fix: New user profile design

--- Explorer/src/app/feature-modules/stakeholder/edit-profile/edit-profile.component.ts
+++ Explorer/src/app/feature-modules/stakeholder/edit-profile/edit-profile.component.ts
@@ -1,32 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Person } from '../model/person.model';
-import { StakeholderService } from '../stakeholder.service';
-import { User } from 'src/app/infrastructure/auth/model/user.model';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/infrastructure/auth/auth.service';
-import { PersonUpdate } from '../model/person-update.model';
+import { Component, OnInit } from "@angular/core";
+import { StakeholderService } from "../stakeholder.service";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/infrastructure/auth/auth.service";
+import { PersonUpdate } from "../model/person-update.model";
 @Component({
-  selector: 'xp-edit-profile',
-  templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css'],
+    selector: "xp-edit-profile",
+    templateUrl: "./edit-profile.component.html",
+    styleUrls: ["./edit-profile.component.css"],
 })
 export class EditProfileComponent implements OnInit {
-  person: PersonUpdate;
-  constructor(private service: StakeholderService,private router: Router, private authService:AuthService) {}
+    person: PersonUpdate;
+    constructor(
+        private service: StakeholderService,
+        private router: Router,
+        private authService: AuthService,
+    ) {}
 
-  ngOnInit(): void {
-    this.authService.user$.subscribe((user) => {
-        this.service.getByUserId(user.id).subscribe((result: PersonUpdate) => {
-          this.person = result;
+    ngOnInit(): void {
+        this.authService.user$.subscribe(user => {
+            this.service
+                .getByUserId(user.id)
+                .subscribe((result: PersonUpdate) => {
+                    this.person = result;
+                });
         });
-      });
-  }
+    }
 
-
-
-  editProfile() {
-    this.service.updatePerson(this.person).subscribe((result) => {
-      this.router.navigate(['/profile']);
-    });
-  }
+    editProfile() {
+        this.service.updatePerson(this.person).subscribe(result => {
+            this.router.navigate(["/profile"]);
+        });
+    }
 }
