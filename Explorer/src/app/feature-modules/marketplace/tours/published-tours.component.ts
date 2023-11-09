3abada71abbfb5b9730b6e013bb2b0aa7d5f6345
Jelena Kovač <jelena.kovac765@gmail.com>
2023-11-09 22:22:08 +0100

    feat: PublishedToursView

--- Explorer/src/app/feature-modules/marketplace/tours/published-tours.component.ts
+++ Explorer/src/app/feature-modules/marketplace/tours/published-tours.component.ts
@@ -1,52 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { PagedResults } from 'src/app/shared/model/paged-results.model';
-import { MarketplaceService } from '../marketplace.service';
-import { Tour } from '../../tour-authoring/model/tour.model';
-import { KeyPoint } from '../../tour-authoring/model/key-point.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { PagedResults } from "src/app/shared/model/paged-results.model";
+import { MarketplaceService } from "../marketplace.service";
+import { Tour } from "../../tour-authoring/model/tour.model";
+import { KeyPoint } from "../../tour-authoring/model/key-point.model";
+import { TourLimitedView } from "../model/tour-limited-view.model";
+import { MatDialog } from "@angular/material/dialog";
+import { Review } from "../model/review.model";
+import { ReviewCardComponent } from "../../layout/review-cards/review-card.component";
 
 @Component({
-  selector: 'xp-published-tours',
-  templateUrl: './published-tours.component.html',
-  styleUrls: ['./published-tours.component.css']
+    selector: "xp-published-tours",
+    templateUrl: "./published-tours.component.html",
+    styleUrls: ["./published-tours.component.css"],
 })
-
 export class PublishedToursComponent implements OnInit {
+    @Input() inputTours: TourLimitedView[] = [];
+    publishedTours: TourLimitedView[] = [];
 
-  publishedTours: Tour[] = [];
-  keyPoint: KeyPoint;
+    constructor(
+        private service: MarketplaceService,
+        public dialogRef: MatDialog,
+    ) {}
 
-  constructor(private service: MarketplaceService) {}
-
-  ngOnInit(): void {
-    this.getPublishedTours();
-  }
-
-  getPublishedTours(): void {
-    this.service.getPublishedTours().subscribe({
-      next: (result: PagedResults<Tour>) =>{
-        this.publishedTours = result.results;
-
-        for(let pt of this.publishedTours){
-          this.getFirstKeyPointForTour(pt);
-        }
-      },
-      error:(err: any) => {
-        console.log(err);
-      }
-    })
-  }
-
-  getFirstKeyPointForTour(tour: Tour): void {
-    if(tour && tour.id){
-      this.service.getToursFirstKeyPoint(tour.id).subscribe({
-        next: (result: KeyPoint) => {
-          this.keyPoint = result;    
-          tour.keyPoints?.push(this.keyPoint);      
-        },
-        error: (err: any) => {
-          console.log(err);
-        },
-      });
+    ngOnInit(): void {
+        //enabling input
+        if (this.inputTours.length === 0) this.getPublishedTours();
+        else this.publishedTours = this.inputTours;
     }
-  };
+
+    getPublishedTours(): void {
+        this.service.getPublishedTours().subscribe({
+            next: (result: PagedResults<Tour>) => {
+                this.publishedTours = result.results;
+            },
+            error: (err: any) => {
+                console.log(err);
+            },
+        });
+    }
+    openDialog(input: Review[]) {
+        console.log(input);
+        const dialogRef = this.dialogRef.open(ReviewCardComponent, {
+            //data: this.listaJavnihTacaka, // lista javnih tacaka koju dobijam u ovoj komponenti i ovim je saljem u modalni dijalog
+            height: "400px",
+            width: "600px",
+            data: {
+                reviews: input,
+            },
+        });
+    }
 }
