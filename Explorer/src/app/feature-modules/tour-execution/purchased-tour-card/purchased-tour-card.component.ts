0d84c9e01d4edc97d7eb6ccd95237b6d2bf36328
IvanPartalo <ivanpartalo123@gmail.com>
2023-11-14 16:31:13 +0100

    feat: showing tour image

--- Explorer/src/app/feature-modules/tour-execution/purchased-tour-card/purchased-tour-card.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/purchased-tour-card/purchased-tour-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Tour } from '../../tour-authoring/model/tour.model';
 import { Router } from '@angular/router';
 import { TourExecutionService } from '../tour-execution.service';
+import { environment } from 'src/env/environment';
 
 @Component({
   selector: 'xp-purchased-tour-card',
@@ -12,10 +13,12 @@ export class PurchasedTourCardComponent implements OnInit{
   @Input() tour: Tour;
   @Input() hasActiveTour: boolean;
   @Input() activeTourId: number;
+  tourImage: string
   isTourActive: boolean = false
   constructor(private router: Router, private service: TourExecutionService){}
   ngOnInit(): void {
     this.CheckIfTourIsActive()
+    this.tourImage = environment.imageHost + this.tour.keyPoints![0].imagePath
   }
   StartTour(){
     this.service.startTour(this.tour.id!).subscribe(() => {
