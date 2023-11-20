96cfe9df8288d253ac05e94acc42a633f883fc73
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2023-11-20 21:47:00 +0100

    style: Edit tour form

--- Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
@@ -19,6 +19,7 @@ import { PublicKeyPointsComponent } from "./public-key-points/public-key-points.
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatDialogModule } from "@angular/material/dialog";
 import { FacilityCardComponent } from "./facility-card/facility-card.component";
+import { EditTourFormComponent } from './edit-tour-form/edit-tour-form.component';
 
 @NgModule({
     declarations: [
@@ -32,6 +33,7 @@ import { FacilityCardComponent } from "./facility-card/facility-card.component";
         KeyPointCardComponent,
         PublicKeyPointsComponent,
         FacilityCardComponent,
+        EditTourFormComponent,
     ],
     imports: [
         CommonModule,
