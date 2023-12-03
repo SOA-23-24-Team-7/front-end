dbf49e88d9cb545221f19b51383f320ac7801e26
Bosko Kulusic <kulusicbosko@gmail.com>
2023-12-03 14:18:41 +0100

    feat: Create campaign from tours

--- Explorer/src/app/feature-modules/tour-execution/purchased-tour-card/purchased-tour-card.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/purchased-tour-card/purchased-tour-card.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Tour } from '../../tour-authoring/model/tour.model';
-import { Router } from '@angular/router';
-import { TourExecutionService } from '../tour-execution.service';
-import { environment } from 'src/env/environment';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Tour } from "../../tour-authoring/model/tour.model";
+import { Router } from "@angular/router";
+import { TourExecutionService } from "../tour-execution.service";
+import { environment } from "src/env/environment";
 import { MatDialog } from "@angular/material/dialog";
 import { KeyPointsViewComponent } from "../key-points-view/key-points-view.component";
 
@@ -11,38 +11,46 @@ import { KeyPointsViewComponent } from "../key-points-view/key-points-view.compo
     templateUrl: "./purchased-tour-card.component.html",
     styleUrls: ["./purchased-tour-card.component.css"],
 })
-export class PurchasedTourCardComponent implements OnInit{
-  @Input() tour: Tour;
-  @Input() hasActiveTour: boolean;
-  @Input() activeTourId: number;
-  tourImage: string
-  isTourActive: boolean = false
-  constructor(private router: Router, private service: TourExecutionService, public dialogRef: MatDialog){}
-  ngOnInit(): void {
-    this.CheckIfTourIsActive()
-    this.tourImage = environment.imageHost + this.tour.keyPoints![0].imagePath
-  }
-  StartTour(){
-    this.service.startTour(this.tour.id!).subscribe(() => {
-      this.router.navigate(['/tour-executing/' + this.tour.id]);
-    });
-  }
-  ContinueTour(){
-    this.router.navigate(['/tour-executing/' + this.tour.id]);
-  }
-  CheckIfTourIsActive(){
-    if(this.hasActiveTour){
-      if(this.tour.id == this.activeTourId){
-        this.isTourActive = true
-      }
+export class PurchasedTourCardComponent implements OnInit {
+    @Input() tour: Tour;
+    @Input() hasActiveTour: boolean;
+    @Input() activeTourId: number;
+    @Output() onSelected = new EventEmitter<any>();
+    tourImage: string;
+    isTourActive: boolean = false;
+    constructor(
+        private router: Router,
+        private service: TourExecutionService,
+        public dialogRef: MatDialog,
+    ) {}
+    ngOnInit(): void {
+        this.CheckIfTourIsActive();
+        this.tourImage =
+            environment.imageHost + this.tour.keyPoints![0].imagePath;
     }
-  }
-  ShowKeyPoints() {
-    const dialogRef = this.dialogRef.open(KeyPointsViewComponent, {
-        data: {
-            keyPoints: this.tour.keyPoints,
-        },
-    });
-      
+    StartTour() {
+        this.service.startTour(this.tour.id!).subscribe(() => {
+            this.router.navigate(["/tour-executing/" + this.tour.id]);
+        });
+    }
+    ContinueTour() {
+        this.router.navigate(["/tour-executing/" + this.tour.id]);
+    }
+    CheckIfTourIsActive() {
+        if (this.hasActiveTour) {
+            if (this.tour.id == this.activeTourId) {
+                this.isTourActive = true;
+            }
+        }
+    }
+    ShowKeyPoints() {
+        const dialogRef = this.dialogRef.open(KeyPointsViewComponent, {
+            data: {
+                keyPoints: this.tour.keyPoints,
+            },
+        });
+    }
+    onSelectedTour(tour: Tour) {
+        this.onSelected.emit(tour);
     }
 }
