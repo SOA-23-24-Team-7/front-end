037cfe76c355f54f9b6d346b7fa6a34615a41916
gagi155 <maslac.ra151.2020@gmail.com>
2023-12-05 12:54:43 +0100

    bugFix: buy tour arguments changed

--- Explorer/src/app/feature-modules/tour-authoring/recommended-tour-card/recommended-tour-card.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/recommended-tour-card/recommended-tour-card.component.ts
@@ -1,63 +1,73 @@
-import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { environment } from 'src/env/environment';
-import { KeyPointsViewComponent } from '../../tour-execution/key-points-view/key-points-view.component';
-import { TourExecutionService } from '../../tour-execution/tour-execution.service';
-import { Tour } from '../model/tour.model';
-import { MarketplaceService } from '../../marketplace/marketplace.service';
+import { Component, Input } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { environment } from "src/env/environment";
+import { KeyPointsViewComponent } from "../../tour-execution/key-points-view/key-points-view.component";
+import { TourExecutionService } from "../../tour-execution/tour-execution.service";
+import { Tour } from "../model/tour.model";
+import { MarketplaceService } from "../../marketplace/marketplace.service";
+import { AuthService } from "src/app/infrastructure/auth/auth.service";
+import { User } from "src/app/infrastructure/auth/model/user.model";
 
 @Component({
-  selector: 'xp-recommended-tour-card',
-  templateUrl: './recommended-tour-card.component.html',
-  styleUrls: ['./recommended-tour-card.component.css']
+    selector: "xp-recommended-tour-card",
+    templateUrl: "./recommended-tour-card.component.html",
+    styleUrls: ["./recommended-tour-card.component.css"],
 })
 export class RecommendedTourCardComponent {
-  @Input() tour: Tour;
-  @Input() hasActiveTour: boolean;
-  @Input() activeTourId: number;
-  tourImage: string
-  isTourActive: boolean = false
-  
-  constructor(private router: Router, private service: MarketplaceService, public dialogRef: MatDialog){}
-  
-  ngOnInit(): void {
-    //this.CheckIfTourIsActive()
-    this.tourImage = environment.imageHost + this.tour.keyPoints![0].imagePath
-  }
-  
-  /*StartTour(){
+    @Input() tour: Tour;
+    @Input() hasActiveTour: boolean;
+    @Input() activeTourId: number;
+    tourImage: string;
+    isTourActive: boolean = false;
+    user: User;
+
+    constructor(
+        private router: Router,
+        private authService: AuthService,
+        private service: MarketplaceService,
+        public dialogRef: MatDialog,
+    ) {}
+
+    ngOnInit(): void {
+        //this.CheckIfTourIsActive()
+        this.authService.user$.subscribe(user => {
+            this.user = user;
+        });
+        this.tourImage =
+            environment.imageHost + this.tour.keyPoints![0].imagePath;
+    }
+
+    /*StartTour(){
     this.service.startTour(this.tour.id!).subscribe(() => {
       this.router.navigate(['/tour-executing/' + this.tour.id]);
     });
   }*/
-  
-  /*ContinueTour(){
+
+    /*ContinueTour(){
     this.router.navigate(['/tour-executing/' + this.tour.id]);
   }*/
-  
-  /*CheckIfTourIsActive(){
+
+    /*CheckIfTourIsActive(){
     if(this.hasActiveTour){
       if(this.tour.id == this.activeTourId){
         this.isTourActive = true
       }
     }
   }*/
-  
-  BuyTour(): void{
-    if(this.tour.id != null){
-      this.service.addToken(this.tour.id);
-      alert("You have successfully bought the tour!");
+
+    BuyTour(): void {
+        if (this.tour.id != null) {
+            this.service.addToken(this.tour.id, this.user.id);
+            alert("You have successfully bought the tour!");
+        }
     }
-  }
 
-
-  ShowKeyPoints() {
-    const dialogRef = this.dialogRef.open(KeyPointsViewComponent, {
-        data: {
-            keyPoints: this.tour.keyPoints,
-        },
-    });
-      
+    ShowKeyPoints() {
+        const dialogRef = this.dialogRef.open(KeyPointsViewComponent, {
+            data: {
+                keyPoints: this.tour.keyPoints,
+            },
+        });
     }
 }
--- Explorer/src/app/feature-modules/tour-execution/secret-popup/secret-popup.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/secret-popup/secret-popup.component.ts
@@ -1,23 +1,25 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
-import { KeyPoint } from '../../tour-authoring/model/key-point.model';
-import { environment } from 'src/env/environment';
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+    MAT_DIALOG_DATA,
+    MatDialog,
+    MatDialogRef,
+} from "@angular/material/dialog";
 @Component({
-  selector: 'xp-secret-popup',
-  templateUrl: './secret-popup.component.html',
-  styleUrls: ['./secret-popup.component.css']
+    selector: "xp-secret-popup",
+    templateUrl: "./secret-popup.component.html",
+    styleUrls: ["./secret-popup.component.css"],
 })
-export class SecretPopupComponent implements OnInit{
-  secret: string = ''
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    public dialog: MatDialogRef<SecretPopupComponent>,
-    public dialogRef: MatDialog
- ) { }
- ngOnInit(): void {
-  this.secret = this.data.dataKey
-}
- onClose(): void {
-  this.dialog.close();
-}
+export class SecretPopupComponent implements OnInit {
+    secret: string = "";
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialog: MatDialogRef<SecretPopupComponent>,
+        public dialogRef: MatDialog,
+    ) {}
+    ngOnInit(): void {
+        this.secret = this.data.dataKey;
+    }
+    onClose(): void {
+        this.dialog.close();
+    }
 }
