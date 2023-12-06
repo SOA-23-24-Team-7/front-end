62b4a20547f1059ea968f25989c45466aee5688b
MilosMilutinovic01 <milutinovicgit@gmail.com>
2023-12-06 14:31:57 +0100

    feat: Added map when creating encounter

--- Explorer/src/app/feature-modules/encounter/encounter-form/encounter-form.component.ts
+++ Explorer/src/app/feature-modules/encounter/encounter-form/encounter-form.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { EncounterService } from "../encounter.service";
 import { Encounter } from "../model/encounter.model";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MapModalComponent } from "src/app/shared/map-modal/map-modal.component";
+import { LocationCoords } from "src/app/shared/model/location-coords.model";
 
 @Component({
     selector: "xp-encounter-form",
@@ -9,7 +12,21 @@ import { Encounter } from "../model/encounter.model";
     styleUrls: ["./encounter-form.component.css"],
 })
 export class EncounterFormComponent {
-    constructor(private service: EncounterService) {}
+    constructor(private service: EncounterService, public dialog: MatDialog) {
+        this.encounterCoords = {
+            longitude: 0,
+            latitude: 0,
+        };
+        this.imageCoords = {
+            longitude: 0,
+            latitude: 0,
+        };
+    }
+    dialogRef: MatDialogRef<MapModalComponent, any> | undefined;
+    imageCoords: LocationCoords;
+    encounterCoords: LocationCoords;
+    selectedEncounter: string;
+    selectedImage: string;
 
     encounterForm = new FormGroup({
         title: new FormControl(undefined, [Validators.required]),
@@ -41,16 +58,16 @@ export class EncounterFormComponent {
             id: 0,
             title: this.encounterForm.value.title || "",
             description: this.encounterForm.value.description || "",
-            longitude: 45.45,
-            latitude: 45.45,
+            longitude: this.encounterCoords.longitude || 0,
+            latitude: this.encounterCoords.latitude || 0,
             radius: this.encounterForm.value.radius || 10,
             xpReward: this.encounterForm.value.xp || 1,
-            status: 2,
-            type: 0,
+            status: 0,
+            type: this.encounterForm.value.selectedStatus,
             peopleNumber: this.encounterForm.value.peopleNumber || 1,
             picture: this.encounterForm.value.pictureURL || "",
-            pictureLongitude: this.encounterForm.value.pictureLongitude || 0,
-            pictureLatitude: this.encounterForm.value.pictureLatitude || 0,
+            pictureLongitude: this.imageCoords.longitude || 0,
+            pictureLatitude: this.imageCoords.latitude || 0,
         };
 
         if (this.encounterType == 1) {
@@ -83,5 +100,50 @@ export class EncounterFormComponent {
                 },
             });
         }
+    }
+
+    selectPictureOnMap() {
+        if (this.dialogRef) {
+            this.dialogRef.close();
+            return;
+        }
+        this.dialogRef = this.dialog.open(MapModalComponent);
+        this.dialogRef.componentInstance.positionChanged.subscribe(
+            (result: LocationCoords) => {
+                this.imageCoords = result;
+                this.selectedImage =
+                    "Longitude: " +
+                    this.encounterCoords.longitude.toString() +
+                    "<br>Latitude: " +
+                    this.encounterCoords.latitude.toString();
+                this.dialogRef = undefined;
+            },
+        );
+        this.dialogRef.afterClosed().subscribe(result => {
+            this.dialogRef = undefined;
+        });
+    }
+
+    selectEncounterOnMap() {
+        if (this.dialogRef) {
+            this.dialogRef.close();
+            return;
+        }
+        this.dialogRef = this.dialog.open(MapModalComponent);
+        this.dialogRef.componentInstance.positionChanged.subscribe(
+            (result: LocationCoords) => {
+                this.encounterCoords = result;
+                this.selectedEncounter =
+                    "Longitude: " +
+                    this.encounterCoords.longitude.toString() +
+                    "<br>Latitude: " +
+                    this.encounterCoords.latitude.toString();
+                this.dialogRef = undefined;
+            },
+        );
+
+        this.dialogRef.afterClosed().subscribe(result => {
+            this.dialogRef = undefined;
+        });
     }
 }
