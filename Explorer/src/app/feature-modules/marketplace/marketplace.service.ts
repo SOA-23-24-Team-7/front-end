c64b315d03b375bc139ebd439d0a56372b3a7e09
Bosko Kulusic <kulusicbosko@gmail.com>
2023-12-23 21:16:31 +0100

    feat: subscribe to daily digest

--- Explorer/src/app/feature-modules/marketplace/marketplace.service.ts
+++ Explorer/src/app/feature-modules/marketplace/marketplace.service.ts
@@ -31,6 +31,8 @@ import { CouponApplication } from "./model/coupon-applicaton.model";
 import { Bundle } from "../tour-authoring/model/bundle.model";
 import { BundleOrderItem } from "./model/bundle-order-item.model";
 import { SortOption } from "./model/sort-option.model";
+import { Subscription } from "./model/subscription.model";
+import { Person } from "../stakeholder/model/person.model";
 
 @Injectable({
     providedIn: "root",
@@ -40,26 +42,39 @@ export class MarketplaceService {
 
     getTourPreference(): Observable<TourPreference> {
         return this.http.get<TourPreference>(
-            environment.apiHost + "tourist/tour-preferences",
+            environment.apiHost + "tourist/preferences",
         );
     }
 
     addPreference(tourPreference: TourPreference): Observable<TourPreference> {
         return this.http.post<TourPreference>(
-            environment.apiHost + "tourist/tour-preferences/create",
+            environment.apiHost + "tourist/preferences/create",
             tourPreference,
+        );
+    }
+
+    addSub(sub: Subscription): Observable<Subscription> {
+        return this.http.post<Subscription>(
+            environment.apiHost + "tourist/subscriber",
+            sub,
+        );
+    }
+
+    getByUserId(userId: number): Observable<Person> {
+        return this.http.get<Person>(
+            environment.apiHost + "people/person/" + userId,
         );
     }
 
     deletePreference(id: number): Observable<TourPreference> {
         return this.http.delete<TourPreference>(
-            environment.apiHost + "tourist/tour-preferences/" + id,
+            environment.apiHost + "tourist/preferences/" + id,
         );
     }
 
     updatePreference(preference: TourPreference): Observable<TourPreference> {
         return this.http.put<TourPreference>(
-            environment.apiHost + "tourist/tour-preferences",
+            environment.apiHost + "tourist/preferences",
             preference,
         );
     }
@@ -385,7 +400,10 @@ export class MarketplaceService {
         return this.http.get<boolean>(route);
     }
 
-    searchTours(searchFilter: any, sortOption: SortOption): Observable<PagedResults<Tour>> {
+    searchTours(
+        searchFilter: any,
+        sortOption: SortOption,
+    ): Observable<PagedResults<Tour>> {
         let query = this.prepareSearchQuery(searchFilter, sortOption);
         console.log(query);
         const path = environment.apiHost + "tourist/tour/search" + query;
@@ -393,52 +411,108 @@ export class MarketplaceService {
     }
 
     prepareSearchQuery(searchFilter: any, sortOption: SortOption): String {
-        let query = `?page=${searchFilter.page}&pageSize=${searchFilter.pageSize}`
+        let query = `?page=${searchFilter.page}&pageSize=${searchFilter.pageSize}`;
         query += searchFilter.name != "" ? `&name=${searchFilter.name}` : "";
-        query += searchFilter.minPrice >= 0 && searchFilter.minPrice !== "" ? `&minPrice=${searchFilter.minPrice}` : "";
-        query += searchFilter.maxPrice >= 0  && searchFilter.maxPrice !== "" ? `&maxPrice=${searchFilter.maxPrice}` : "";
-        query += searchFilter.onDiscount != false ? `&onDiscount=${searchFilter.onDiscount}` : "";
-        query += searchFilter.minDifficulty >= 0  && searchFilter.minDifficulty !== "" ? `&minDifficulty=${searchFilter.minDifficulty}` : "";
-        query += searchFilter.maxDifficulty >= 0  && searchFilter.maxDifficulty !== "" ? `&maxDifficulty=${searchFilter.maxDifficulty}` : "";
-        query += searchFilter.minDuration >= 0 && searchFilter.minDuration !== "" ? `&minDuration=${searchFilter.minDuration}` : "";
-        query += searchFilter.maxDuration >= 0 && searchFilter.maxDuration !== "" ? `&maxDuration=${searchFilter.maxDuration}` : "";
-        query += searchFilter.minAverageRating >= 0 && searchFilter.minAverageRating !== "" ? `&minAverageRating=${searchFilter.minAverageRating}` : "";
-        query += searchFilter.minLength >= 0 && searchFilter.minLength !== "" ? `&minLength=${searchFilter.minLength}` : "";
-        query += searchFilter.maxLength >= 0 && searchFilter.maxLength !== "" ? `&maxLength=${searchFilter.maxLength}` : "";
-        query += searchFilter.longitude >= -180 && searchFilter.longitude !== "" ? `&longitude=${searchFilter.longitude}` : "";
-        query += searchFilter.latitude >= -180 && searchFilter.latitude !== "" ? `&latitude=${searchFilter.latitude}` : "";
-        query += searchFilter.distance > 0 && searchFilter.distance !== "" ? `&maxDistance=${searchFilter.distance}` : "";
+        query +=
+            searchFilter.minPrice >= 0 && searchFilter.minPrice !== ""
+                ? `&minPrice=${searchFilter.minPrice}`
+                : "";
+        query +=
+            searchFilter.maxPrice >= 0 && searchFilter.maxPrice !== ""
+                ? `&maxPrice=${searchFilter.maxPrice}`
+                : "";
+        query +=
+            searchFilter.onDiscount != false
+                ? `&onDiscount=${searchFilter.onDiscount}`
+                : "";
+        query +=
+            searchFilter.minDifficulty >= 0 && searchFilter.minDifficulty !== ""
+                ? `&minDifficulty=${searchFilter.minDifficulty}`
+                : "";
+        query +=
+            searchFilter.maxDifficulty >= 0 && searchFilter.maxDifficulty !== ""
+                ? `&maxDifficulty=${searchFilter.maxDifficulty}`
+                : "";
+        query +=
+            searchFilter.minDuration >= 0 && searchFilter.minDuration !== ""
+                ? `&minDuration=${searchFilter.minDuration}`
+                : "";
+        query +=
+            searchFilter.maxDuration >= 0 && searchFilter.maxDuration !== ""
+                ? `&maxDuration=${searchFilter.maxDuration}`
+                : "";
+        query +=
+            searchFilter.minAverageRating >= 0 &&
+            searchFilter.minAverageRating !== ""
+                ? `&minAverageRating=${searchFilter.minAverageRating}`
+                : "";
+        query +=
+            searchFilter.minLength >= 0 && searchFilter.minLength !== ""
+                ? `&minLength=${searchFilter.minLength}`
+                : "";
+        query +=
+            searchFilter.maxLength >= 0 && searchFilter.maxLength !== ""
+                ? `&maxLength=${searchFilter.maxLength}`
+                : "";
+        query +=
+            searchFilter.longitude >= -180 && searchFilter.longitude !== ""
+                ? `&longitude=${searchFilter.longitude}`
+                : "";
+        query +=
+            searchFilter.latitude >= -180 && searchFilter.latitude !== ""
+                ? `&latitude=${searchFilter.latitude}`
+                : "";
+        query +=
+            searchFilter.distance > 0 && searchFilter.distance !== ""
+                ? `&maxDistance=${searchFilter.distance}`
+                : "";
         query += sortOption != SortOption.NoSort ? `&sortBy=${sortOption}` : "";
         return query;
     }
 
     addTourSale(tourSale: TourSale): Observable<TourSale> {
-      return this.http.post<TourSale>(environment.apiHost + "tour-sales", tourSale);
+        return this.http.post<TourSale>(
+            environment.apiHost + "tour-sales",
+            tourSale,
+        );
     }
 
     getTourSales(): Observable<TourSale[]> {
-      return this.http.get<TourSale[]>(environment.apiHost + "tour-sales");
+        return this.http.get<TourSale[]>(environment.apiHost + "tour-sales");
     }
 
     getTourSaleById(id: number): Observable<TourSale> {
-      return this.http.get<TourSale>(environment.apiHost + "tour-sales/" + id);
+        return this.http.get<TourSale>(
+            environment.apiHost + "tour-sales/" + id,
+        );
     }
 
     updateTourSale(tourSale: TourSale): Observable<TourSale> {
-      return this.http.put<TourSale>(environment.apiHost + "tour-sales", tourSale);
+        return this.http.put<TourSale>(
+            environment.apiHost + "tour-sales",
+            tourSale,
+        );
     }
 
     deleteTourSale(id: number): Observable<void> {
-      return this.http.delete<void>(environment.apiHost + "tour-sales/" + id);
+        return this.http.delete<void>(environment.apiHost + "tour-sales/" + id);
     }
 
     getDiscountForTour(tourId: number): Observable<number | null> {
-        return this.http.get<number | null>(environment.apiHost + "tour-sales/tours/" + tourId);
+        return this.http.get<number | null>(
+            environment.apiHost + "tour-sales/tours/" + tourId,
+        );
     }
 
-    getPublishedToursByAuthor(authorId: number): Observable<PagedResults<Tour>> {
-      const path = environment.apiHost + "tourist/tour/search" + "?page=0&pageSize=0&authorId=" + authorId;
-      return this.http.get<PagedResults<Tour>>(path);
+    getPublishedToursByAuthor(
+        authorId: number,
+    ): Observable<PagedResults<Tour>> {
+        const path =
+            environment.apiHost +
+            "tourist/tour/search" +
+            "?page=0&pageSize=0&authorId=" +
+            authorId;
+        return this.http.get<PagedResults<Tour>>(path);
     }
 
     addCoupon(coupon: Coupon): Observable<Coupon> {
@@ -472,21 +546,23 @@ export class MarketplaceService {
         let path = environment.apiHost + "tourist/bundles";
         return this.http.get<Bundle[]>(path);
     }
-    
+
     getBundleById(bundleId: number): Observable<Bundle> {
         let path = environment.apiHost + "tourist/bundles/" + bundleId;
         return this.http.get<Bundle>(path);
     }
-    
+
     removeBundleOrderItem(bundleOrderItemId: number): Observable<any> {
-        let path = environment.apiHost + "tourist/shoppingCart/remove-bundle-item/" + bundleOrderItemId;
+        let path =
+            environment.apiHost +
+            "tourist/shoppingCart/remove-bundle-item/" +
+            bundleOrderItemId;
         return this.http.delete<any>(path);
     }
-    
+
     buyBundle(bundleId: number): Observable<any> {
-        console.log('6');
+        console.log("6");
         let path = environment.apiHost + "token/bundle/" + bundleId;
         return this.http.post<any>(path, {});
-
     }
 }
--- Explorer/src/app/feature-modules/marketplace/tour-preferences/tour-preferences.component.ts
+++ Explorer/src/app/feature-modules/marketplace/tour-preferences/tour-preferences.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { TourPreference } from '../model/tour-preference.model'
-import { MarketplaceService } from '../marketplace.service';
-import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { TourPreference } from "../model/tour-preference.model";
+import { Subscription } from "../model/subscription.model";
+import { MarketplaceService } from "../marketplace.service";
+import { AuthService } from "src/app/infrastructure/auth/auth.service";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Subscriber } from "rxjs";
+import { User } from "src/app/infrastructure/auth/model/user.model";
+import { Person } from "../../stakeholder/model/person.model";
 
 @Component({
-  selector: 'xp-tour-preferences',
-  templateUrl: './tour-preferences.component.html',
-  styleUrls: ['./tour-preferences.component.css']
+    selector: "xp-tour-preferences",
+    templateUrl: "./tour-preferences.component.html",
+    styleUrls: ["./tour-preferences.component.css"],
 })
 export class TourPreferencesComponent implements OnInit {
+    isEditing: Boolean = false;
+    selectedFrequency: number = 1;
 
-  isEditing: Boolean = false
-
-  preference: TourPreference = {
+    preference: TourPreference = {
         id: -1,
         userId: -1,
         difficultyLevel: -1,
@@ -20,45 +25,80 @@ export class TourPreferencesComponent implements OnInit {
         cyclingRating: 0,
         carRating: 0,
         boatRating: 0,
-        selectedTags: []}
+        selectedTags: [],
+    };
 
-  constructor(private service: MarketplaceService, private authService: AuthService) { }
+    subscriber: Subscription = {
+        touristId: -1,
+        emailAddress: "",
+        frequency: 0,
+        lastTimeSent: "",
+    };
+    person: Person;
+    constructor(
+        private service: MarketplaceService,
+        private authService: AuthService,
+    ) {}
 
-  ngOnInit(): void {
-      this.service.getTourPreference().subscribe({
-        next: (result: TourPreference) => {
-          this.preference = result;
-        },
-        error: (err: any) => {
-        }
-      })
-  }
+    ngOnInit(): void {
+        this.service.getTourPreference().subscribe({
+            next: (result: TourPreference) => {
+                this.preference = result;
+            },
+            error: (err: any) => {},
+        });
+        this.authService.user$.subscribe(user => {
+            if (!user.id) return;
+            this.service
+                .getByUserId(this.authService.user$.getValue().id)
+                .subscribe(result => {
+                    this.person = result;
+                    this.subscriber.emailAddress = result.email;
+                    this.subscriber.touristId = result.userId;
+                });
+        });
+    }
 
-  deleteEquipment(id: number): void {
-    this.service.deletePreference(id).subscribe({
-      next: () => {
-        this.getPreference();
-      },
-    })
-  }
+    deleteEquipment(id: number): void {
+        this.service.deletePreference(id).subscribe({
+            next: () => {
+                this.getPreference();
+            },
+        });
+    }
 
-  getPreference(): void {
-    this.service.getTourPreference().subscribe({
-      next: (result: TourPreference) => {
-        this.preference = result;
-      },
-      error: (error) => {
-          this.preference.id = -1;
-      }
-    })
-  }
+    getPreference(): void {
+        this.service.getTourPreference().subscribe({
+            next: (result: TourPreference) => {
+                this.preference = result;
+            },
+            error: error => {
+                this.preference.id = -1;
+            },
+        });
+    }
 
-  onEditClicked(preference: TourPreference): void {
-    this.preference = preference;
-  }
+    onEditClicked(preference: TourPreference): void {
+        this.preference = preference;
+    }
 
-  onEditChange(shouldEdit: boolean): void {
-    this.isEditing = shouldEdit;
-  }
-  
+    onEditChange(shouldEdit: boolean): void {
+        this.isEditing = shouldEdit;
+    }
+
+    addSub(): void {
+        console.log(this.selectedFrequency);
+        this.subscriber.frequency = this.selectedFrequency;
+        let yourDate = new Date();
+        const offset = yourDate.getTimezoneOffset();
+        yourDate = new Date(yourDate.getTime() - offset * 60 * 1000);
+        this.subscriber.lastTimeSent = yourDate.toISOString().split("T")[0];
+        //this.subscriber.lastTimeSent = "2002-12-12";
+        console.log(this.subscriber);
+        this.service.addSub(this.subscriber).subscribe({
+            next: (result: Subscription) => {
+                console.log(result);
+            },
+        });
+    }
 }
