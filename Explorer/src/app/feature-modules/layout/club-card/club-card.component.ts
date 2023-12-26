89c9e0f713aeac577a6dd3f9c35273a71dfebd5c
Marko Cekerinac <marko.cekerinac@gmail.com>
2023-12-26 18:13:34 +0100

    feat: My clubs view, update and delete

--- Explorer/src/app/feature-modules/layout/club-card/club-card.component.ts
+++ Explorer/src/app/feature-modules/layout/club-card/club-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { MarketplaceService } from "../../marketplace/marketplace.service";
 import { AuthService } from "src/app/infrastructure/auth/auth.service";
 import { MyClubJoinRequest } from "../../marketplace/model/my-club-join-request.model";
@@ -7,9 +7,12 @@ import { PagedResults } from "src/app/shared/model/paged-results.model";
 import {
     faDoorOpen,
     faCheck,
-    faEnvelope
-  } from "@fortawesome/free-solid-svg-icons";
+    faEnvelope,
+    faEdit,
+    faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { ClubMember } from "../../marketplace/model/club-member.model";
+import { Club } from "../../marketplace/model/club.model";
 
 @Component({
     selector: "xp-club-card",
@@ -17,6 +20,8 @@ import { ClubMember } from "../../marketplace/model/club-member.model";
     styleUrls: ["./club-card.component.css"],
 })
 export class ClubCardComponent {
+    @Output() editClubClicked = new EventEmitter<Club>();
+    @Output() deleteClubClicked = new EventEmitter<Club>();
     user: User;
     @Input() club: any;
     myClubJoinRequests: MyClubJoinRequest[] = [];
@@ -24,55 +29,69 @@ export class ClubCardComponent {
     faDoorOpen = faDoorOpen;
     faCheck = faCheck;
     faEnvelope = faEnvelope;
+    faEdit = faEdit;
+    faTrash = faTrash;
 
     constructor(
-        private service: MarketplaceService, 
-        private authService: AuthService
-      ) {}
+        private service: MarketplaceService,
+        private authService: AuthService,
+    ) {}
 
     ngOnInit(): void {
         this.authService.user$.subscribe(user => {
             this.user = user;
         });
-        this.getClubJoinRequests()
+        this.getClubJoinRequests();
         this.getMembers();
     }
 
     onImageError(event: Event) {
         const target = event.target as HTMLImageElement;
         if (target) {
-          target.src = "https://imgs.search.brave.com/udmDGOGRJTYO6lmJ0ADA03YoW4CdO6jPKGzXWvx1XRI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzY4LzU1LzYw/LzM2MF9GXzI2ODU1/NjAxMl9jMVdCYUtG/TjVyalJ4UjJleVYz/M3puSzRxblllS1pq/bS5qcGc";
+            target.src =
+                "https://imgs.search.brave.com/udmDGOGRJTYO6lmJ0ADA03YoW4CdO6jPKGzXWvx1XRI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzY4LzU1LzYw/LzM2MF9GXzI2ODU1/NjAxMl9jMVdCYUtG/TjVyalJ4UjJleVYz/M3puSzRxblllS1pq/bS5qcGc";
         }
     }
 
     getClubJoinRequests(): void {
         this.service.getMyClubJoinRequests().subscribe({
-          next: (result: PagedResults<MyClubJoinRequest>) => {
-            this.myClubJoinRequests = result.results;
-          },
-          error: (errData) => {
-            console.log(errData)
-          }
-        })
+            next: (result: PagedResults<MyClubJoinRequest>) => {
+                this.myClubJoinRequests = result.results;
+            },
+            error: errData => {
+                console.log(errData);
+            },
+        });
     }
 
     getMembers(): void {
         this.service.getClubMembers(this.club.id).subscribe({
-          next: (result: PagedResults<ClubMember>) => {
-            this.members = result.results;
-          },
-          error: (errData) => {
-            console.log(errData)
-          }
-        })
+            next: (result: PagedResults<ClubMember>) => {
+                this.members = result.results;
+            },
+            error: errData => {
+                console.log(errData);
+            },
+        });
     }
 
     canSendJoinRequest(): boolean {
-        return !this.myClubJoinRequests.some(joinRequest => joinRequest.status === 'Pending' && joinRequest.clubId == this.club.id);
+        return !this.myClubJoinRequests.some(
+            joinRequest =>
+                joinRequest.status === "Pending" &&
+                joinRequest.clubId == this.club.id,
+        );
     }
 
     isMember(): boolean {
-        return this.members.some(member => member.id === this.user.id) || (this.club.ownerId == this.user.id);
+        return (
+            this.members.some(member => member.id === this.user.id) &&
+            this.club.ownerId !== this.user.id
+        );
+    }
+
+    isMine(): boolean {
+        return this.club.ownerId == this.user.id;
     }
 
     sendJoinRequest(): void {
@@ -80,9 +99,16 @@ export class ClubCardComponent {
             next: () => {
                 this.getClubJoinRequests();
             },
-          error: (errData) => {
-            console.log(errData)
-          }
-        })
-      }
+            error: errData => {
+                console.log(errData);
+            },
+        });
+    }
+
+    updateClub(club: Club): void {
+        this.editClubClicked.emit(club);
+    }
+    deleteClub(club: Club): void {
+        this.deleteClubClicked.emit(club);
+    }
 }
