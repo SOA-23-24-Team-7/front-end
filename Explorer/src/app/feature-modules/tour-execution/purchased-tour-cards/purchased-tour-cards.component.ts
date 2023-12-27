13a395280e6559c7dbd5e0ffa6e53aa0f209290c
mvelimir <velimirmilinkovic1@gmail.com>
2023-12-27 17:30:54 +0100

    feat: Update purchased tour card

--- Explorer/src/app/feature-modules/tour-execution/purchased-tour-cards/purchased-tour-cards.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/purchased-tour-cards/purchased-tour-cards.component.ts
@@ -8,11 +8,25 @@ import { CampaignCreate } from "../model/campaign-create.model";
 import { AuthService } from "src/app/infrastructure/auth/auth.service";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Campaign } from "../model/campaign-info.model";
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: "xp-purchased-tour-cards",
     templateUrl: "./purchased-tour-cards.component.html",
     styleUrls: ["./purchased-tour-cards.component.css"],
+    animations: [
+        trigger('slideInOut', [
+            state('in', style({ transform: 'translateX(0)' })),
+            transition('void => *', [
+                style({ transform: 'translateX(-100%)' }),
+                animate('300ms ease-out')
+            ]),
+            transition('* => void', [
+                animate('300ms ease-in', style({ transform: 'translateX(100%)' }))
+            ])
+        ])
+    ]
 })
 export class PurchasedToursComponent implements OnInit {
     currentIndex: number = 0;
@@ -27,6 +41,7 @@ export class PurchasedToursComponent implements OnInit {
     selectedTours: Tour[] = [];
     selectedTourIds: number[] = [];
     userId: number;
+    selectedTourId: number = 0;
     campaignForm = new FormGroup({
         campaignName: new FormControl("", []),
         campaignDescription: new FormControl("", []),
@@ -34,7 +49,7 @@ export class PurchasedToursComponent implements OnInit {
     constructor(
         private tourExecutionService: TourExecutionService,
         private authService: AuthService,
-    ) {}
+    ) { }
     ngOnInit(): void {
         this.getTours();
         this.getLiveTour();
@@ -50,7 +65,7 @@ export class PurchasedToursComponent implements OnInit {
     getTours() {
         this.tourExecutionService.getTours().subscribe({
             next: (result: Tour[]) => {
-                this.purchasedTours = result.concat(result).concat(result).concat(result);
+                this.purchasedTours = result.concat(result).concat(result).concat(result).concat(result).concat(result);
             },
             error: (err: any) => {
                 console.log(err);
@@ -87,6 +102,10 @@ export class PurchasedToursComponent implements OnInit {
         if (tour.id) this.selectedTourIds.push(tour.id);
         console.log(tour.name);
     }
+    onDeselectedTour(tour: Tour) {
+        this.selectedTours = this.selectedTours.filter(item => item !== tour);
+        console.log(tour.name);
+    }
     CancelCampaignCreating() {
         this.campaignForm.reset({
             campaignDescription: "",
@@ -96,6 +115,7 @@ export class PurchasedToursComponent implements OnInit {
         this.selectedTours.length = 0;
     }
     createCampaign() {
+        alert(JSON.stringify(this.campaignForm.value))
         if (
             this.campaignForm.value.campaignDescription == "" ||
             this.campaignForm.value.campaignName == ""
@@ -161,4 +181,6 @@ export class PurchasedToursComponent implements OnInit {
         this.CampaignsContainer!.scrollLeft -=
             this.CampaignsContainer.children[this.currentIndex].clientWidth;
     }
+
+    faXmark = faXmark;
 }
