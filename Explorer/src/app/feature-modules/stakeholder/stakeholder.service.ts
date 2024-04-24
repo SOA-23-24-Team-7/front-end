dee2a500cbc05d64cd23240d3d46f1702356c4b3
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-04-24 16:52:49 +0100

    added followers and followings

--- Explorer/src/app/feature-modules/stakeholder/stakeholder.service.ts
+++ Explorer/src/app/feature-modules/stakeholder/stakeholder.service.ts
@@ -65,13 +65,13 @@ export class StakeholderService {
             environment.apiHost + "people",
         );
     }
-    getFollowers(id: number): Observable<PagedResults<Follower>> {
-        return this.http.get<PagedResults<Follower>>(
+    getFollowers(id: number): Observable<any> {
+        return this.http.get<Follower[]>(
             environment.apiHost + "follower/followers/" + id,
         );
     }
-    getFollowings(id: number): Observable<PagedResults<Following>> {
-        return this.http.get<PagedResults<Following>>(
+    getFollowings(id: number): Observable<any> {
+        return this.http.get<Follower[]>(
             environment.apiHost + "follower/followings/" + id,
         );
     }
@@ -80,14 +80,15 @@ export class StakeholderService {
             environment.apiHost + "follower/search/" + searchUsername,
         );
     }
-    deleteFollowing(follow: FollowerCreate): Observable<Following> {
-        return this.http.post<Following>(
+    deleteFollowing(follow: FollowerCreate): Observable<any> {
+        return this.http.post<any>(
             environment.apiHost + "follower/unfollow",follow,
         );
     }
-    addFollowing(follow: FollowerCreate): Observable<any> {
-        return this.http.post<any>(
-            environment.apiHost + "follower/follow", follow,
+    addFollowing(follow: FollowerCreate): Observable<FollowerCreate> {
+        return this.http.post<FollowerCreate>(
+            environment.apiHost + "follower/follow",
+            follow,
         );
     }
 
